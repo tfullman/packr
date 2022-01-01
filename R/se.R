b4a77339421e6bfdb6fc1133d08f9b644c169544
Tim Fullman <fullman.tim@gmail.com>
2021-12-31 16:14:45 -0900

    Add NA handling to se()

--- R/se.R
+++ R/se.R
@@ -1,12 +1,17 @@
 #' Standard error of a sample mean
 #'
+#' Calculates the standard error of a sample mean. Note `NA` values are
+#' automatically omitted by default.
+#'
 #' @param x Vector of numbers
 #'
 #' @return Numeric value
 #' @export
 #'
-#' @examples se(1:5)
+#' @examples
+#' se(1:5)
+#' se(c(1:3,NA,4:5))
 se <-
   function(x){
-    stats::sd(x) / sqrt(length(x))
+    stats::sd(x, na.rm=TRUE) / sqrt(length(stats::na.omit(x)))
   }
